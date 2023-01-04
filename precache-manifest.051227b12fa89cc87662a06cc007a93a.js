721dd993e00712afd8382348cdc6eede0e788256
rdcleminson@gmail.com <root@R-Machine.localdomain>
2023-01-04 17:22:00 +1100

    Updates

---
+++ precache-manifest.051227b12fa89cc87662a06cc007a93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "10e1741cfd181b07f801",
+    "url": "/React-Portfolio/static/js/main.10e1741c.chunk.js"
+  },
+  {
+    "revision": "d55e317000ddf8141ba8",
+    "url": "/React-Portfolio/static/js/1.d55e3170.chunk.js"
+  },
+  {
+    "revision": "10e1741cfd181b07f801",
+    "url": "/React-Portfolio/static/css/main.87277b96.chunk.css"
+  },
+  {
+    "revision": "d55e317000ddf8141ba8",
+    "url": "/React-Portfolio/static/css/1.9446c69b.chunk.css"
+  },
+  {
+    "revision": "6f29cf4c486b9ab301fc0a4d23c719a3",
+    "url": "/React-Portfolio/index.html"
+  }
+];
